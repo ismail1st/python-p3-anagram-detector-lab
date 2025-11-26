45d3dd0818eac413914e8eab310fd6344047f8dc
Ismail Hassan <ismaylhassan8@gmail.com>
2025-11-26 10:19:37 +0300

    lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        target = sorted(self.word)
+        return [w for w in word_list if sorted(w.lower()) == target]
